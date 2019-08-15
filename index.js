fbaec459a131b2f8f3f182303b15f38c72bf72f1
Kyle Benson <kylebenson@kyles-mbp.lan>
2019-08-15 18:27:28 -0400

    Update express app to look at DB

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 import express from 'express';
 
-
 const fetch = require('node-fetch');
 const app = express();
 const PORT = 5000;
 const cors = require('cors');
 
-const attendeeNames = [
-    {"first": "Kyle", "last": "Benson" },
-    {"first": "Colin", "last": "McNaughton"},
-    {"first": "Brian", "last": "Coursen"}
-]
+var mysql = require('mysql')
+
+
+
+// const attendeeNames = [
+//     {"first": "Kyle", "last": "Benson" },
+//     {"first": "Colin", "last": "McNaughton"},
+//     {"first": "Brian", "last": "Coursen"}
+// ]
 
 app.use(cors());
 
 app.get('/', (req, res)=> {
     console.log("We have a new request")
-    res.send(attendeeNames)
+    var connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: DB_USER,
+      password: DB_PASS,
+      database: DB_DB
+    })
+    connection.query('SELECT first_name, last_name FROM attendance WHERE attendee_type="Attendee"', function(err, rows, fields) {
+    connection.end();
+      if (!err) 
+        return res.send(rows);
+    else
+      console.log('Error while performing Query.');
+      return;
+    }); 
 });
 
 app.listen(PORT, ()=>
